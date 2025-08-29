c7a1bade255958448a82c5faad0ccc2c53735bbf
LavaTime <amitkeren9@gmail.com>
2025-08-29 12:55:43 +0300

    Parse ethernet type, and prepare to action on each supported type

--- main/main.c
+++ main/main.c
@@ -2,9 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <arpa/inet.h>
 
 #define MAC_ADDRESS_LEN 6
 #define ETHERNET_TYPE_LEN 2
+#define ETHER_ARP_TYPE 0x0806
+#define ETHER_IPV4_TYPE 0x0800
+
 #define BUFLEN 100
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 unsigned char EXAMPLE_PACKET[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xd4, 0x92, 0x5e, 0x7c, 0x3d, 0x2f, 0x8, 0x6, 0x0, 0x1, 0x8, 0x0, 0x6, 0x4, 0x0, 0x1, 0xd4, 0x92, 0x5e, 0x7c, 0x3d, 0x2f, 0xa, 0x0, 0x0, 0x7c, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x0, 0xd0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
@@ -36,6 +40,23 @@ struct ethernet_hdr
     uint16_t ethernet_type;
 };
 
+char *determine_ether_type(uint16_t ethernet_type)
+{
+    uint16_t ethernet_type_converted = ntohs(ethernet_type);
+    switch (ethernet_type_converted)
+    {
+    case ETHER_ARP_TYPE:
+        return "ARP";
+        break;
+    case ETHER_IPV4_TYPE:
+        return "IPv4";
+        break;
+    default:
+        return "Unknown";
+        break;
+    };
+}
+
 int main(int argc, char *argv[])
 {
     char *packet_current_position_ptr;
@@ -49,6 +70,25 @@ int main(int argc, char *argv[])
     printf("Source MAC: ");
     print_mac_address_split_by_colons(eth_hdr->src_mac);
     puts("\n");
+
+    printf("Ethernet Type: %04x\n", ntohs(eth_hdr->ethernet_type));
+
+    char *ether_type_name = determine_ether_type(eth_hdr->ethernet_type);
+    printf("Ethernet Type: %s\n", ether_type_name);
+
+    switch (ntohs(eth_hdr->ethernet_type))
+    {
+    case ETHER_ARP_TYPE:
+        /* code */
+        break;
+    case ETHER_IPV4_TYPE:
+        // code
+        break;
+    default:
+        // Unknown type
+        return 1;
+        break;
+    }
 
     return 0;
 }
